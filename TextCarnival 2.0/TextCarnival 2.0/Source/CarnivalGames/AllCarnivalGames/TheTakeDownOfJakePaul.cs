cf69f6f1179a7d749aed09460e085392200602e5
Impact245 <Beasley821@gmail.com>
2017-09-26 11:31:44 -0400

    tictac

---
+++ TextCarnival 2.0/TextCarnival 2.0/Source/CarnivalGames/AllCarnivalGames/TheTakeDownOfJakePaul.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextCarnivalV2.Source.CarnivalGames.AllCarnivalGames
+{
+    class TheTakeDownOfJakePaul : CarnivalGame
+
+    {
+        public TheTakeDownOfJakePaul() : base()
+        {
+
+        }
+
+        public override string getName()
+        {
+            return "When The Haters Dab Back!?!?";
+        }
+
+    }
+}
