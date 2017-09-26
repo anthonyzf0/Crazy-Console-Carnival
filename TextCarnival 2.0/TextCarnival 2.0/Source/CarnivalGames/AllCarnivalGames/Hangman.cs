ee0ed8352d73630c9f40e5d5f10df2d835a1694a
khouryj <khouryj6@gmail.com>
2017-09-26 11:32:31 -0400

    Hangman

---
+++ TextCarnival 2.0/TextCarnival 2.0/Source/CarnivalGames/AllCarnivalGames/Hangman.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextCarnivalV2.Source.CarnivalGames.AllCarnivalGames
+{
+    class Hangman : CarnivalGame
+    {
+        public Hangman() : base()
+        {
+
+        }
+
+        public override string getName()
+        {
+            return "Hangman";
+        }
+
+        public override void play()
+        {
+            
+        }
+    }
+}
