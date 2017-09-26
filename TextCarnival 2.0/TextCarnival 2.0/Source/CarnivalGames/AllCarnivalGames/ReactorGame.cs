13ea6dd3ba798a1dcff4f2005d4c434e164955dc
MatthewSmickle <matt.smickle.o@gmail.com>
2017-09-26 11:30:31 -0400

    Beginning Reaction Game

---
+++ TextCarnival 2.0/TextCarnival 2.0/Source/CarnivalGames/AllCarnivalGames/ReactorGame.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextCarnivalV2.Source.CarnivalGames.AllCarnivalGames
+{
+    class ReactorGame : CarnivalGame
+    {
+        public ReactorGame() : base()
+        {
+
+        }
+
+        public override string getName()
+        {
+            return "Reaction Game";
+        }
+
+        public override void play()
+        {
+            showTitle("WELCOME TO THE BEST REACTION GAME EVER");
+            writeLine("A random letter will appear and you will have to type it as fast as you can.");
+            writeLine("You have to type the letter than HIT ENTER");
+
+            writeLine("Ready?");
+            bool ready = getYesNo();
+
+            if(!ready)
+            {
+
+            }
+            
+        }
+    }
+}
