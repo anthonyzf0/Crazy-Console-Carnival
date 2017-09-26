dfc1e076304f91a481f50ddb3d9a689f42394f38
BeckettMorsch <beckettmorsch@gmail.com>
2017-09-26 11:30:46 -0400

    started game

---
+++ TextCarnival 2.0/TextCarnival 2.0/Source/CarnivalGames/AllCarnivalGames/TurnBasedRPG.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextCarnivalV2.Source.CarnivalGames.AllCarnivalGames
+{
+    class TurnBasedRPG : CarnivalGame
+    {
+        public TurnBasedRPG() : base()
+        {
+
+        }
+
+        public override string getName()
+        {
+            return "Turn Based RPG";
+        }
+
+        public override void play()
+        {
+            // do work here
+            writeOut("CHOOSE A CLASS! ");
+            String choice = getOption("fire", "Run away", "ORC");
+            List<String> abilities;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
