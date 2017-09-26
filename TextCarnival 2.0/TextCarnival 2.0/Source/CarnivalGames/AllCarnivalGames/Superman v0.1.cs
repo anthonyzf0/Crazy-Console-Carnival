2cb111a333aa8b964adf2051ed80e8ba02e114e3
ChristianMohan <christianjmohan@gmail.com>
2017-09-26 11:31:32 -0400

    Adventure Game v 0.1
    - Added in a game

---
+++ TextCarnival 2.0/TextCarnival 2.0/Source/CarnivalGames/AllCarnivalGames/Superman v0.1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextCarnivalV2.Source.CarnivalGames.AllCarnivalGames
+{
+    class Superman_v0 : CarnivalGame
+    {
+        public override string getName()
+        {
+            return "Tell your Tale Game";
+        }
+
+        public override void play()
+        {
+            showTitle("Welcome to ___");
+            writeLine("This is a text based adventure game.");
+            writeLine("The choices you make will change what happens next");
+        }
+    }
+}
