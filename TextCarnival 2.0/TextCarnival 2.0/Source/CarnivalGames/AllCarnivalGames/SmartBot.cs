af5342bb73ce759b2dd0704e9b523a9db2fbaabc
Spazdude2 <spaziani96@gmail.com>
2017-09-26 11:35:01 -0400

    Start of smartbot

---
+++ TextCarnival 2.0/TextCarnival 2.0/Source/CarnivalGames/AllCarnivalGames/SmartBot.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace TextCarnivalV2.Source.CarnivalGames.AllCarnivalGames
+{
+    class SmartBot : CarnivalGame
+    {
+        public override string getName()
+        {
+            return "SmartBot";
+        }
+
+        public override void play()
+        {
+            showTitle("WELCOME TO SMARTBOT");
+
+            writeOut("Hello, my name is SmartBot. What is yours?");
+            write("|a0> ");
+            String name = Console.ReadLine();
+
+            writeOut("Hello " + name + ", are you ejoying this?\n[Yes] or [No]");
+            String response = Console.ReadLine();
+
+            response = response.ToLower();
+
+            if(response != "yes" || response != "no")
+            {
+                writeOut("");
+            }
+
+
+        }
+    }
+}
+        
