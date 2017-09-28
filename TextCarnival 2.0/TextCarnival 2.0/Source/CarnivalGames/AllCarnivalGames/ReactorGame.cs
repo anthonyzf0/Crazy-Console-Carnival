f6bfdb256f50d61259183d45343487a17fb3f9cf
MatthewSmickle <matt.smickle.o@gmail.com>
2017-09-28 12:16:01 -0400

    did something

--- TextCarnival 2.0/TextCarnival 2.0/Source/CarnivalGames/AllCarnivalGames/ReactorGame.cs
+++ TextCarnival 2.0/TextCarnival 2.0/Source/CarnivalGames/AllCarnivalGames/ReactorGame.cs
@@ -22,14 +22,21 @@ namespace TextCarnivalV2.Source.CarnivalGames.AllCarnivalGames
         {
             showTitle("WELCOME TO THE BEST REACTION GAME EVER");
             writeLine("A random letter will appear and you will have to type it as fast as you can.");
-            writeLine("You have to type the letter than HIT ENTER");
+          
 
             writeLine("Ready?");
             bool ready = getYesNo();
 
+            Random rnd = new Random();
+            int letter = rnd.Next(1, 27);
+
             if(!ready)
             {
-
+                writeLine("Aww to bad. You don't play the game. You lose. Good day sir. GG NO RE.");
+            }
+            else
+            {
+                writeLine(letter+ "");
             }
             
         }
