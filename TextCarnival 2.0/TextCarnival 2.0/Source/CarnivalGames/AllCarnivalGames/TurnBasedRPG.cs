9ac150e7fe26c0dd953627d805ca94263c4d4b44
BeckettMorsch <beckettmorsch@gmail.com>
2017-09-28 11:32:51 -0400

    continue

--- TextCarnival 2.0/TextCarnival 2.0/Source/CarnivalGames/AllCarnivalGames/TurnBasedRPG.cs
+++ TextCarnival 2.0/TextCarnival 2.0/Source/CarnivalGames/AllCarnivalGames/TurnBasedRPG.cs
@@ -20,13 +20,74 @@ namespace TextCarnivalV2.Source.CarnivalGames.AllCarnivalGames
 
         public override void play()
         {
-            // do work here
+            // class choice
             writeOut("CHOOSE A CLASS! ");
-            String choice = getOption("fire", "Run away", "ORC");
-            List<String> abilities;
+            writeOut("Mage or Warrior");
+            String choice = getOption("Mage", "Warrior");
+            
+            // variables
+            String abName1;
+            String abName2;
+            int roomNum = 0;
+            String option;
+            int health = 100;
+
+            // ability set up
+            if ( choice == "Mage")
+            {
+                abName1 = "Fireball";
+                abName2 = "Staff Pummle";
+            }
+            else
+            {
+                abName1 = "Slash";
+                abName2 = "Sheild Bash";
+            }
+
+            // intro
+            writeOut("You are a travler seeking adventure and fortune..... You wlaked through a tunnle into a cave......  You know that there is a treasure in thses parts, and you have made it to the dungeon.");
 
 
 
+            // writeOut("Do you turn left or right");
+            // option = getOption("left", "right");
+
+            /*/
+            public String LorF(String op)
+            {
+
+
+                if (op == "left")
+                {
+                    return roomNum--;
+                }
+                else
+                {
+                    return roomNum++;
+                }
+            }
+
+            public int encounter(int num)
+            {
+                if(num == 0)
+                {
+                    writeLine("You are in the entrance once again ");
+
+                }
+
+                if(num == -1)
+                {
+                    writeLine("");
+                }
+
+
+            }
+            /*/
+
+
+            writeOut("You appear in the next room, and you are faced with 4 maddened goblins. They are prepared to take you out of their home by force.");
+            wait(1);
+            writeOut("You are now in combat");
 
 
 
