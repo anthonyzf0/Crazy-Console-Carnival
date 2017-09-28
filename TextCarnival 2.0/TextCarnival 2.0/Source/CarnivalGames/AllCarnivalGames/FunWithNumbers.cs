a6deab2f5461dd5bfefb93abec3248ca5b4b851d
ChrisPettinari <chrispettinari@gmail.com>
2017-09-28 12:41:45 -0400

    hey do you like numbers because i sure haha yes

---
+++ TextCarnival 2.0/TextCarnival 2.0/Source/CarnivalGames/AllCarnivalGames/FunWithNumbers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextCarnivalV2.Source.CarnivalGames.AllCarnivalGames
+{
+    class FunWithNumbers : CarnivalGame
+    {
+        public FunWithNumbers() : base()
+        {
+
+        }
+
+        public override string getName()
+        {
+            return "Fun With Numbers";
+        }
+
+        public override void play()
+        {
+            writeOut("I'm so glad you came to visit");
+        }
+    }
+
+}
