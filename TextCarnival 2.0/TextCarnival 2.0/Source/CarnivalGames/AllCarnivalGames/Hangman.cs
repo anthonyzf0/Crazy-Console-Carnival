d62ae4ef434d67e26e9412e87b2f97969b522d5f
khouryj <khouryj6@gmail.com>
2017-09-28 11:33:55 -0400

    Hangman

--- TextCarnival 2.0/TextCarnival 2.0/Source/CarnivalGames/AllCarnivalGames/Hangman.cs
+++ TextCarnival 2.0/TextCarnival 2.0/Source/CarnivalGames/AllCarnivalGames/Hangman.cs
@@ -8,6 +8,11 @@ namespace TextCarnivalV2.Source.CarnivalGames.AllCarnivalGames
 {
     class Hangman : CarnivalGame
     {
+        int tries = 5;
+        String word;
+        String word1 = "";
+        String letters = "";
+
         public Hangman() : base()
         {
 
@@ -25,10 +30,35 @@ namespace TextCarnivalV2.Source.CarnivalGames.AllCarnivalGames
             writeOut("I'm just going to assume you already know what hangman is and how to play...(you need at least a second person)...");
 
             writeOut("Have one person choose a word for you to guess: ");
-            String word = getInput();
+            word = getInput();
 
             clear();
-            write("-----\nl    l\nl\nl\nl\nl\n-----------");
+            foreach (char c in word)
+            {
+                word1 += "-";
+            }
+        }
+
+        public void update()
+        {
+            writeLine("Tries: " + tries);
+            writeLine("\n" + word1 + "\n\n" + letters);
+            writeLine("[guess phrase] or [guess letter]");
+            String input = getOption("guess phrase", "guess letter");
+            if (input == "guess letter")
+            {
+                write("\nEnter a letter (that hasn't been used): ");
+                input = getInput();
+                if (word.Contains(input) && !letters.Contains(input))
+                {
+                    word1.
+                }
+            }
+            else
+            {
+                write("\nGuess the phrase: ");
+                input = getInput();
+            }
         }
     }
 }
