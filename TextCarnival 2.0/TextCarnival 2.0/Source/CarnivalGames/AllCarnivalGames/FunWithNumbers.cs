7bd8926b087a76f3176f455614288e829d138db2
ChrisPettinari <chrispettinari@gmail.com>
2017-10-04 11:33:01 -0400

    uh iss ya boi skinny nipples

--- TextCarnival 2.0/TextCarnival 2.0/Source/CarnivalGames/AllCarnivalGames/FunWithNumbers.cs
+++ TextCarnival 2.0/TextCarnival 2.0/Source/CarnivalGames/AllCarnivalGames/FunWithNumbers.cs
@@ -20,7 +20,116 @@ namespace TextCarnivalV2.Source.CarnivalGames.AllCarnivalGames
 
         public override void play()
         {
-            writeOut("I'm so glad you came to visit");
+            writeOut("haha i hope you like numbers idiot, anyway solve these maffs");
+
+            Random rng = new Random();
+
+            int num1 = rng.Next(1, 100);
+            int num2 = rng.Next(1, 100);
+            int oper = rng.Next(0, 4);
+            int un = rng.Next(0, 10);
+
+            String unit = "";
+
+            if (un == 0)
+            {
+                unit = " poop(s)";
+            }
+
+            if(un == 1)
+            {
+                unit = " Matt Smickle(s)";
+            }
+
+            if(un == 2)
+            {
+                unit = " buncc(s)";
+            }
+
+            if (un == 3)
+            {
+                unit = " snek(s)";
+            }
+
+            if (un == 4)
+            {
+                unit = " monkey(s)";
+            }
+
+            if (un == 5)
+            {
+                unit = " minkey(s)";
+            }
+
+            if (un == 6)
+            {
+                unit = " shamwow(s)";
+            }
+
+            if (un == 7)
+            {
+                unit = " quncc(s)";
+            }
+
+            if (un == 8)
+            {
+                unit = " time(s) ive wanted to kms";
+            }
+
+            if (un == 9)
+            {
+                unit = " butt(s)";
+            }
+
+
+            if (un == 10)
+            {
+                unit = " tuncc(s)";
+            }
+
+            int solution = 69;
+            if (oper == 3)
+            {
+                while (num1 % num2 != 0)
+                {
+                    num1 = rng.Next(1, 100);
+                    num2 = rng.Next(1, 100);
+                }
+                solution = num1 / num2;
+
+                writeOut(num1 + unit + " / " + num2 + unit + "?  (Only write the number)");
+            }
+
+            if (oper == 2)
+            {
+                solution = num1 * num2;
+
+                writeOut(num1 + unit + " * " + num2 + unit + "?  (Only write the number)");
+            }
+
+            if (oper == 1)
+            {
+                solution = num1 - num2;
+
+                writeOut(num1 + unit + " - " + num2 + unit + "?  (Only write the number)");
+            }
+
+            if (oper == 0)
+            {
+                solution = num1 + num2;
+
+                writeOut(num1 + unit + " + " + num2 + unit + "?  (Only write the number)");
+            }
+            int input = Convert.ToInt32(getInput());
+
+            if (input == solution)
+            {
+                writeOut("nice job you got it right");
+            }
+            else
+            {
+                writeOut("you are poop");
+            }
         }
     }
 
