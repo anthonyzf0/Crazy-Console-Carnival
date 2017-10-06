ce72c15a0029c237b45bbd12fac9ce676f93bb3a
Impact245 <Beasley821@gmail.com>
2017-10-06 11:25:03 -0400

    Tic Dab Toe v2.2

--- TextCarnival 2.0/TextCarnival 2.0/Source/CarnivalGames/AllCarnivalGames/TheTakeDownOfJakePaul.cs
+++ TextCarnival 2.0/TextCarnival 2.0/Source/CarnivalGames/AllCarnivalGames/TheTakeDownOfJakePaul.cs
@@ -24,10 +24,39 @@ namespace TextCarnivalV2.Source.CarnivalGames.AllCarnivalGames
             showTitle("Welcome Tic Dab Toe");
 
             writeOut("DAB ON THEM HATERS!!!!!");
-            writeOut("X will start");
+            writeOut("Player 1 will be Haters, Player 2 will be Dab.");
+            writeOut("Haters will start.");
+            
+
+            String[,] board = new String[3,3];
+
+            
+
+                for (int i = 0; i < board.GetLength(0); i++)
+                {
+                    for (int j = 0; j < board.GetLength(1); j++)
+                    {
+                        board[i, j] = "Dab";
+                        write(board[i, j]);
+                    }
+                }
+
+                
+                
+            
+        }
+
+        public void placement()
+        {
+            int r, c, n;
+            writeOut("Where will you place your word? Ex. (player# row column)");
+            String[] place = Console.ReadLine().Split(' ');
+
+            n = Convert.ToInt32(place[0]);
+            r = Convert.ToInt32(place[1]);
+            c = Convert.ToInt32(place[2]);
 
 
-          
         }
     }
 }
