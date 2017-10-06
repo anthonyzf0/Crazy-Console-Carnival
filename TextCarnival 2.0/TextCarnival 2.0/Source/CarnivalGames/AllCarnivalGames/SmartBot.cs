3217b79fbfe7fd74dc168b48d7815b6dfd7fc397
Spazdude2 <spaziani96@gmail.com>
2017-10-06 11:07:12 -0400

    Updated

--- TextCarnival 2.0/TextCarnival 2.0/Source/CarnivalGames/AllCarnivalGames/SmartBot.cs
+++ TextCarnival 2.0/TextCarnival 2.0/Source/CarnivalGames/AllCarnivalGames/SmartBot.cs
@@ -27,20 +27,25 @@ namespace TextCarnivalV2.Source.CarnivalGames.AllCarnivalGames
             writeOut("Hello, my name is SmartBot. What is yours?");
             write("|a0> ");
             String name = getInput();
+            writeLine("");
 
             writeOut("Hello " + name + ", are you enjoying this?\n[Yes] or [No]");
             write("|a0> ");
             bool response = getYesNo();
+            writeLine("");
 
-            if(response)
+            if (response)
             {
                 writeOut("That's great " + name);
                 write("|a0> ");
                 userResponse = getInput();
+                writeLine("");
             }
 
             while (true)
             {
+                
+
                 List<String> Jokes = new List<string>() { "Why couldn't the blond add 10 and 7 on a calculator?\n\nBecause she couldn't find the 10 key",
                             "What did one penny say to another penny?\n\nLet's get together and make cents!",
                             "What do you call someone with a mental deficiency?\n\nRetarded!"};
@@ -50,15 +55,19 @@ namespace TextCarnivalV2.Source.CarnivalGames.AllCarnivalGames
                     writeOut("You're welcome!");
                     write("|a0> ");
                     userResponse = getInput();
+                    writeLine("");
                 }
 
                 if (userResponse == "be a comedian")
                 {
+                    clear();
+
                     int jokes = 3;
 
                     writeOut("Hello, " + name + " and welcome to 'Comedian Mode' where all your dreams come true. You will get a list of 10 random jokes, once the 10 jokes have been told, you will reenter 'Normal Mode'.");
                     write("|a0> ");
                     userResponse = getInput();
+                    writeLine("");
 
                     while (jokes > 0)
                     {
@@ -70,6 +79,7 @@ namespace TextCarnivalV2.Source.CarnivalGames.AllCarnivalGames
                         writeOut(Jokes[0]);
                         write("|a0> ");
                         userResponse = getInput();
+                        writeLine("");
 
 
                         Jokes.RemoveAt(0);
@@ -85,6 +95,7 @@ namespace TextCarnivalV2.Source.CarnivalGames.AllCarnivalGames
                     writeOut(SBtext[rand.Next(0, 3)]);
                     write("|a0> ");
                     userResponse = getInput();
+                    writeLine("");
                 }
             }
 
