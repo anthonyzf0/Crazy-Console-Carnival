6f1d566a391a7c117742d75c7bc6a636fd8776c5
MatthewSmickle <matt.smickle.o@gmail.com>
2017-10-12 11:18:33 -0400

    finished it again

--- TextCarnival 2.0/TextCarnival 2.0/Source/CarnivalGames/AllCarnivalGames/ReactorGame.cs
+++ TextCarnival 2.0/TextCarnival 2.0/Source/CarnivalGames/AllCarnivalGames/ReactorGame.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,46 +22,145 @@ namespace TextCarnivalV2.Source.CarnivalGames.AllCarnivalGames
         public override void play()
         {
             showTitle("WELCOME TO THE BEST REACTION GAME EVER");
-            writeLine("A random letter will appear and you will have to type it as fast as you can.");
+            writeOut("A random letter will appear and you will have to type it as fast as you can.");
           
 
-            writeLine("Ready?");
-            bool ready = getYesNo();
+            writeOut("What difficulty do you want to play on: " +
+                "\neasy (1 second timer and lowercase letters) " +
+                "\nmedium (1 second timer with lowercase letters, uppercase letters, and numbers)" +
+                "\nhard (1/2 second timer with lowercase letters, uppercase letters, and numbers)");
+            String difficulty = getOption("easy", "medium", "hard");
 
             
 
-            String abc = "abcdefghijklmnopqrstuvwxyz";
+            String abc = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
             int correct = 0;
             int wrong = 0;
-                
-            if(!ready)    
-            {
-                writeLine("Aww to bad. You don't play the game. You lose. Good day sir. GG NO RE.");
-            }
-            if (ready)
-            {
+            Stopwatch time = new Stopwatch();
+            Random rnd = new Random();
 
+           
+            if (difficulty == "easy")
+            {
+                wait(1);
+                writeLine("3");
+                wait(1);
+                writeLine("2");
+                wait(1);
+                writeLine("1");
                 while (wrong < 1)
                 {
-                    Random rnd = new Random();
                     int letter = rnd.Next(1, 27);
-                    if (ready)
+                    writeLine(abc[letter - 1] + "");
+
+                    time.Reset();
+                    time.Start();
+
+                    ConsoleKey key = getKey();
+                    writeLine("");
+
+                    time.Stop();
+
+
+                    
+                    if (key.ToString().ToLower() == abc[letter - 1] + "" && time.ElapsedMilliseconds <= 1000)
                     {
-                        writeLine(abc[letter - 1] + "");
-                        ConsoleKey key = getKey();
-                        writeLine("");
-                       
-                        if (key.ToString().ToLower() == abc[letter - 1] + "")
-                        {
-                          correct++;
-                        }
-                       else
-                        {
-                           wrong++;
-                        }
+                      correct++;
                     }
+                    else
+                    {
+                        writeOut("You're tooooooooo slow! or you got the answer wrong I'm not in the mood to figure it out.");
+                        writeOut("Congrats you got " + correct + " correct!");
+                        writeOut("Ya done now!");
+                        beep();
+                        wrong++;
+                    }
+
+
+                    
                 }
-                writeLine("Congrats you got " + correct + " correct!");
+                
+            }
+            else if( difficulty == "medium")
+            {
+                wait(1);
+                writeLine("3");
+                wait(1);
+                writeLine("2");
+                wait(1);
+                writeLine("1");
+                while (wrong < 1)
+                {
+                    
+                    int letter = rnd.Next(1, 63);
+                    writeLine(abc[letter - 1] + "");
+
+                    time.Reset();
+                    time.Start();
+
+                    ConsoleKey key = getKey();
+                    writeLine("");
+
+                    time.Stop();
+
+
+
+                    if (key.ToString().ToLower() == abc[letter - 1] + "" && time.ElapsedMilliseconds <= 1000)
+                    {
+                        correct++;
+                    }
+                    else
+                    {
+                        writeOut("You're tooooooooo slow! or you got the answer wrong I'm not in the mood to figure it out.");
+                        writeOut("Congrats you got " + correct + " correct!");
+                        writeOut("Ya done now!");
+                        beep();
+                        wrong++;
+                    }
+
+
+
+                }
+            }
+            else if( difficulty == "hard")
+            {
+                wait(1);
+                writeLine("3");
+                wait(1);
+                writeLine("2");
+                wait(1);
+                writeLine("1");
+                while (wrong < 1)
+                {
+                    int letter = rnd.Next(1, 63);
+                    writeLine(abc[letter - 1] + "");
+
+                    time.Reset();
+                    time.Start();
+
+                    ConsoleKey key = getKey();
+                    writeLine("");
+
+                    time.Stop();
+
+
+
+                    if (key.ToString().ToLower() == abc[letter - 1] + "" && time.ElapsedMilliseconds <= 500)
+                    {
+                        correct++;
+                    }
+                    else
+                    {
+                        writeOut("You're tooooooooo slow! or you got the answer wrong I'm not in the mood to figure it out.");
+                        writeOut("Congrats you got " + correct + " correct!");
+                        writeOut("Ya done now!");
+                        beep();
+                        wrong++;
+                    }
+
+
+
+                }
             }
 
 
